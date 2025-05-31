180857d52d4d747878e5f6958e1b2f02f4d4eba9
elmalia <viviahemalia@gmail.com>
2025-05-31 19:31:24 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "vivia elmalia rahman";
+  static String gambar = "assets/gambar_dosen/vivia elmalia rahman.png";
 }
